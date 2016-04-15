5eae18d0e6b39dbf747f355fe7bcbf729d2d7cec
mradambeck <mr.adambeck@gmail.com>
2016-04-15 12:23:51 -0700

    oute working and index is pulling in books titles

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 angular.module('libraryApp', ['ngRoute'])
-       .config(config)
-       .controller('BooksShowController', BooksShowController);
+       .config(config);
 
 ////////////
 // ROUTES //
@@ -10,9 +9,9 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config (  $routeProvider,   $locationProvider  )  {
   $routeProvider
     .when('/', {
-      templateUrl: /* Include the path to the index template */,
-      controller:  /* Which controller do you want the main page to use */,
-      controllerAs:/* What will you call the controller in the html? */
+      templateUrl: '/templates/books/index.html',
+      controller:  'BooksIndexController',
+      controllerAs: 'booksIndexCtrl'
     })
     /* Include the additional route here! */
     .otherwise({
@@ -25,4 +24,4 @@ function config (  $routeProvider,   $locationProvider  )  {
       enabled: true,
       requireBase: false
     });
-};
+}
--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,13 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response) {
+    vm.books = response.data.books;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
 }
